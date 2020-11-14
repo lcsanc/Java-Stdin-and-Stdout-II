ca90fa010d97cfb14bdbf9337dfaeb76c266d7f2
lcsanc <47239003+lcsanc@users.noreply.github.com>
2020-11-13 21:47:36 -0600

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+/*Sample Input
+
+42
+3.1415
+Welcome to HackerRank's Java tutorials!
+Sample Output
+
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt(); 
+        double d = scan.nextDouble(); 
+        String f = scan.nextLine(); 
+        String s = scan.nextLine(); 
+        // you must use two scan next lines because the nextDouble method does not read the newline character in your input created by hitting "Enter," and so the call to Scanner.nextLine returns after reading that newline.       
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
